ce852aff1f20d424f4f09655cf9888629afbddf7
selles--canche <selles--canche@et.esiea.fr>
2026-04-06 20:10:57 +0200

    fix bug and missing explication

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,16 +11,17 @@ sudo pacman -S --noconfirm \
   stow \ # maintained by opensus
   waybar \ # maintained by the community. 
   flatpak \ # maintained by the flatpak team.
-  hyperpaper \ #maintained by the community, need to by checked
+  hyprpaper \ #maintained by the community, need to by checked
   ghostty \ # maintained by the ghostty teams
-  alsa-utils \ 
-  unzip \
+  alsa-utils \  # used by pipewire and pulseaudio (need to be checked), you can also use it for debug your audio at the low level of your pc. 
+  unzip \ # extract archive from zip. 
+  hyprlauncher \ # maintained by the hyprland teams and open a menu to launch applciations and flatpak.
   # pulseaudio \
   brightnessctl \ # maintained by the hyprland team (need to be checked)
-  sof-firmware \ 
-  xdg-desktop-portal \ # maintained by the hyprland team (need to be checked)
-  xdg-desktop-portal-gtk \ # maintained by hyplrand and used for file picker (like flatpak picking file) (need to be checked)
-  xdg-desktop-portal-hyprland \ # maintained by htpland team and is used for screen shared. 
+  sof-firmware \  # maintained by intel and linux community, the packet will downlods the correct firemware for your modern audio controller (like your speaker and your mic).
+  xdg-desktop-portal \ # maintained by the hyprland team (need to be checked). He is like a bridge from your application sandboxed and your desktop environement. 
+  xdg-desktop-portal-gtk \ # maintained by hyplrand and used for file picker (like flatpak picking file) (need to be checked). Same but this is the backend, to display messages from your sandboxed applications to your desktop environement (like notification, file picker, open link etc etc).
+  xdg-desktop-portal-hyprland \ # maintained by htpland team and is used for screen sharing.
   hyprshot \ # maintained by the community, used for take screenshot on hyprland. 
   pipewire \ # Server audio maintained by the community. USE FOR THE WAYLAND AUDIO SCREEN SHARE
   wireplumber \  # maintained by freedesktop.org. Sheldulded all the audio application to isolate each application audio. USE FOR THE WAYLAND AUDIO SREEN SHARE 
