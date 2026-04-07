6d7b7ce8b9aad7e1210f0a084319d9576e173c37
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-04-07 12:39:19 +0200

    fix: change the name of bootstrap.sh to install.sh bc now setup stow inside

--- install.sh
+++ install.sh
@@ -6,9 +6,13 @@ echo "==> Installing dependencies..."
 
 # Core packages
 # TODO, (need to be checked) == check who is the maintainer of the project.
-sudo pacman -S --noconfirm \
-  nwg-displays stow waybar flatpak hyprpaper ghostty alsa-utils unzip hyprlauncher brightnessctl sof-firmware xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprshot pipewire wireplumber
+sudo pacman -S --noconfirm nwg-displays stow waybar flatpak hyprpaper ghostty alsa-utils unzip hyprlauncher brightnessctl sof-firmware xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprshot pipewire wireplumber
 
+echo "==> setup with stow your files in the system"
+sudo rm -rf ~/.config/hypr 
+sudo rm -rf ~/.config/waybar
+stow --target=$HOME hyprland-config
+stow --target=$HOME waybar-config
 
 # Zen Browser (flatpak)
 echo "==> Installing Zen Browser via flatpak..."
@@ -27,3 +31,5 @@ sleep 0.5
 echo "Now enabling services needed for screen shared..."
 sleep 0.5
 systemctl --user enable --now pipewire wireplumber xdg-desktop-portal-hyprland
+
+echo "==> Now rebooting"
