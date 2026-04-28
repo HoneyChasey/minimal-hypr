d26820edbaf491259c0a892784fab7f339b332f0
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-04-28 19:51:25 +0200

    hotfix: install.sh adding hyprlock

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 echo "==> Installing dependencies..."
 
 # Core packages
-sudo pacman -S --noconfirm nwg-displays stow waybar flatpak hyprpaper ghostty alsa-utils unzip hyprlauncher brightnessctl sof-firmware xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprshot pipewire wireplumber 
+sudo pacman -S --noconfirm nwg-displays stow waybar flatpak hyprpaper ghostty alsa-utils unzip hyprlauncher brightnessctl sof-firmware xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-hyprland hyprshot pipewire wireplumber hyprlock
 
 # Adding quickshell-lockscreen dependencies
 sudo pacman -S --noconfirm sddm qt6-declarative qt6-5compat qt6-svg qt6-multimedia qt6-multimedia-ffmpeg gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly
