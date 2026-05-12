135fc24073ca39ad6c803ef9ac94097968bc3e38
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-05-12 12:00:39 +0200

    Quickfix: fix workspace, autostart
    - Fix the move to workspace to the new version in lua thx to reddit
    - autostart fix
    - Update animations

--- hyprland-config/.config/hypr/hyprland/autostart.lua
+++ hyprland-config/.config/hypr/hyprland/autostart.lua
@@ -4,7 +4,7 @@
 -- Or execute your favorite apps at launch like this:
 
 hl.on("hyprland.start", function ()
-  hl.exec_cmd(terminal) -- Application for the status bar. Include also the workspace overview. Find config in ~/.config/waybar/. Don't need to launch at the begining we enable this service.
-  hl.exec_cmd(waybar) --
-  hl.exec_cmd(hyprpaper) -- Application for wallpaper
+  hl.exec_cmd("ghostty") -- Application for the status bar. Include also the workspace overview. Find config in ~/.config/waybar/. Don't need to launch at the begining we enable this service.
+  hl.exec_cmd("waybar") --
+  hl.exec_cmd("hyprpaper") -- Application for wallpaper
 end)
