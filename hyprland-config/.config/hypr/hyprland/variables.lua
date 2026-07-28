30f1fbb406fb697421434d982729b7456b851dcc
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-07-28 11:51:56 +0000

    update to the latest version of ghostty

--- hyprland-config/.config/hypr/hyprland/variables.lua
+++ hyprland-config/.config/hypr/hyprland/variables.lua
@@ -6,9 +6,9 @@
 
 -- See https://wiki.hypr.land/Configuring/Keywords/
 -- global variable
-Terminal = "ghostty"
+Terminal = "gdbus call --session --dest com.mitchellh.ghostty --object-path /com/mitchellh/ghostty --method org.gtk.Actions.Activate new-window '[]' '[]'" --https://github.com/mylinuxforwork/dotfiles/issues/1363 -- delete the '' arround [] if you not using zsh
 FileManager = "dolphin"
 Menu = "bemenu-run -b"
 -- Browser = "flatpak run app.zen_browser.zen"
-Browser = "firefox"
+Browser = "flatpak run org.mozilla.firefox"
 WallapperApp = "hyprpaper"
