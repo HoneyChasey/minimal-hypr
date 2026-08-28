6ecc417a25a339a91016a5ea335b793c2d9c3b58
HoneyChasey <44633479+HoneyChasey@users.noreply.github.com>
2026-08-28 11:46:56 +0200

    Feaute: adding flake to the project

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev shell for minimal-hypr";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            lua-language-server 
+          ];
+        };
+      });
+}
